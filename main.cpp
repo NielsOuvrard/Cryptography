7a2a267dc8299b89a65283b576d3c20df826812c
Martin <leo2.martin@epitech.eu>
2023-11-08 11:16:23 +0100

    :fuse: parsing and test

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@ void print_usage () {
 }
 
 int main (int argc, char* argv[]) {
-    int option;
     bool xor_flag = false;
     bool aes_flag = false;
     bool rsa_flag = false;
@@ -25,46 +24,55 @@ int main (int argc, char* argv[]) {
     bool block_mode_flag = false;
     bool generate_key_flag = false;
     std::string key;
+    std::string input = argv[argc - 1];
 
-    while ((option = getopt(argc, argv, "xor:aes:rsa:cd:bhg:")) != -1) {
-        switch (option) {
-            case 'x':
-                xor_flag = true;
-                break;
-            case 'a':
-                aes_flag = true;
-                break;
-            case 'r':
-                rsa_flag = true;
-                break;
-            case 'c':
-                encrypt_flag = true;
-                break;
-            case 'd':
-                decrypt_flag = true;
-                break;
-            case 'b':
-                block_mode_flag = true;
-                if (!xor_flag && !aes_flag) {
-                    std::cerr << "Block mode (-b) is only valid for -xor and -aes." << std::endl;
-                    exit(EXIT_FAILURE);
-                }
-                break;
-            case 'g':
-                generate_key_flag = true;
-                if (!rsa_flag) {
-                    std::cerr << "Generating keys (-g) is only valid for -rsa." << std::endl;
-                    exit(EXIT_FAILURE);
-                }
-                break;
-            case 'h':
-                print_usage();
-                exit(EXIT_SUCCESS);
-            default:
-                std::cerr << "Invalid option. Use -h for help." << std::endl;
-                exit(EXIT_FAILURE);
+    for (int x = 1; argv[x][0] == '-'; x++) {
+        std::string arg = argv[x];
+        if ("-xor" == arg) {
+            xor_flag = true;
+            break;
         }
+        if ("-aes" == arg) {
+            aes_flag = true;
+            break;
+        }
+        if ("-rsa" == arg) {
+            rsa_flag = true;
+            break;
+        }
+        if ("-c" == arg) {
+            encrypt_flag = true;
+            break;
+        }
+        if ("-d" == arg) {
+            decrypt_flag = true;
+            break;
+        }
+        if ("-b" == arg) {
+            block_mode_flag = true;
+            if (!xor_flag && !aes_flag) {
+                std::cerr << "Block mode (-b) is only valid for -xor and -aes." << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            break;
+        }
+        if ("-help" == arg || "-h" == arg) {
+            print_usage();
+            exit(EXIT_SUCCESS);
+        }
+        std::cerr << "Invalid option. Use -h for help." << std::endl;
+        exit(EXIT_FAILURE);
     }
+
+    std::cout << "xor_flag: " << xor_flag << std::endl;
+    std::cout << "aes_flag: " << aes_flag << std::endl;
+    std::cout << "rsa_flag: " << rsa_flag << std::endl;
+    std::cout << "encrypt_flag: " << encrypt_flag << std::endl;
+    std::cout << "decrypt_flag: " << decrypt_flag << std::endl;
+    std::cout << "block_mode_flag: " << block_mode_flag << std::endl;
+    std::cout << "generate_key_flag: " << generate_key_flag << std::endl;
+    std::cout << "key: " << key << std::endl;
+    std::cout << "input: " << input << std::endl;
 
     if (!xor_flag && !aes_flag && !rsa_flag) {
         std::cerr << "Select an encryption algorithm (-xor, -aes, -rsa)." << std::endl;
@@ -86,6 +94,10 @@ int main (int argc, char* argv[]) {
     } else {
         std::cerr << "KEY is required. Use -h for help." << std::endl;
         exit(EXIT_FAILURE);
+    }
+
+    if (xor_flag && !block_mode_flag) {
+        xorEncryptDecrypt(input, key);
     }
 
     return 0;
