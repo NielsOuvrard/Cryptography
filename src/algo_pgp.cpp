223bec4bc0512400ea29b7ecc22fe575b9a9c59f
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-11-26 20:38:01 +0100

    🌟 random AES key generation for PGP encryption

--- src/algo_pgp.cpp
+++ src/algo_pgp.cpp
@@ -1,6 +1,25 @@
 #include "config.hpp"
+#include <iostream>
+#include <random>
+#include <sstream>
 
-static std::string aes_key = "fff0ff39f06d33cfefe6936430b6c94f";
+std::string generateRandomAESKey()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(0, 15);
+
+    std::ostringstream oss;
+    oss << std::hex;
+
+    for (int i = 0; i < 32; ++i) {
+        oss << dis(gen);
+    }
+
+    return oss.str();
+}
+
+// static std::string aes_key = "fff0ff39f06d33cfefe6936430b6c94f";
 // static std::string rsa_public_key = "010001-c9f91a9ff3bd6d84005b9cc8448296330bd23480f8cf8b36fd4edd0a8cd925de139a0076b962f4d57f50d6f9e64e7c41587784488f923dd60136c763fd602fb3";
 // static std::string rsa_private_key = "81b08f4eb6dd8a4dd21728e5194dfc4e349829c9991c8b5e44b31e6ceee1e56a11d66ef23389be92ef7a4178470693f509c90b86d4a1e1831056ca0757f3e209-c9f91a9ff3bd6d84005b9cc8448296330bd23480f8cf8b36fd4edd0a8cd925de139a0076b962f4d57f50d6f9e64e7c41587784488f923dd60136c763fd602fb3";
 
@@ -10,6 +29,8 @@ std::string pgpEncrypt(const std::string &message, const std::string &key)
     std::pair<inf_int, inf_int> keyPair;
     keyPair.first = str_hexa_to_int(inverse_two_by_two_rev(key.substr(0, key.find("-"))));
     keyPair.second = str_hexa_to_int(inverse_two_by_two_rev(key.substr(key.find("-") + 1, key.length())));
+
+    std::string aes_key = generateRandomAESKey();
 
     std::string encrypted_aes_key = rsaEncrypt(aes_key, keyPair);
     std::string encrypted_message = aesEncrypt(message, aes_key);
